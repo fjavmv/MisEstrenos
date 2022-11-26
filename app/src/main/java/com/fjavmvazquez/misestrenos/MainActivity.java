37be5c8c9fc95701e8a5a514c3b938793d4c7dc7
fjavmv <fjavmv@gmail.com>
2022-11-25 23:34:12 -0600

    Se crea vista inicial de aplicación

--- app/src/main/java/com/fjavmvazquez/misestrenos/MainActivity.java
+++ app/src/main/java/com/fjavmvazquez/misestrenos/MainActivity.java
@@ -3,12 +3,79 @@ package com.fjavmvazquez.misestrenos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView mImageViewRegistrar;
+    ImageView mImageViewConsultar;
+    TextView mTextViewRegistrar;
+    TextView mTextViewConsultar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Ocultamos barras de estado y action bar
+        ocultarBarraDeEstado();
+        //Inicializamos componentes
+        inicializarComponentes();
+        //onClick
+        mImageViewConsultar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                display(view.toString());
+            }
+        });
+
+        mTextViewConsultar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                display(view.toString());
+            }
+        });
+        mImageViewRegistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                display(view.toString());
+            }
+        });
+        mTextViewRegistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                display(view.toString());
+            }
+        });
+
+    }
+
+    public void inicializarComponentes(){
+        mImageViewConsultar = findViewById(R.id.imgvConsultar);
+        mImageViewRegistrar = findViewById(R.id.imgvRegistrar);
+        mTextViewRegistrar = findViewById(R.id.txtRegistrar);
+        mTextViewConsultar = findViewById(R.id.txtConsultar);
+    }
+
+    public void onClick(View v){
+
+    }
+
+
+    public void ocultarBarraDeEstado(){
+        //Permite ocultar el actionBar
+        Objects.requireNonNull(getSupportActionBar()).hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
+
+    public void display(String mensaje){
+        Toast.makeText(this,mensaje,Toast.LENGTH_SHORT).show();
     }
 }
